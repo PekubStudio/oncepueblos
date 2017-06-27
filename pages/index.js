2e393231801bf3a51dffa9f7fa23009fd598d2dd
Saul Flores <saulpolo95@gmail.com>
2017-06-27 13:21:37 -0700

    React Grid System

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Link from 'next/link'
+import { Col, Row } from 'react-grid-system'
 
 export default () => (
   <div>
+    <Row>
+      <Col md={6}>
+        left
+      </Col>
+      <Col md={6}>
+        Right
+      </Col>
+    </Row>
     <h1>Hello I am a <Link href='/about'><a>Pekub</a></Link></h1>
   </div>
 )
